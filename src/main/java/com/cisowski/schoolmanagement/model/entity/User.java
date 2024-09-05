688b60bcf39fc0bcddf33ec28ca683e802d9cdab
PiotrCisowski1 <piotr.cisowski@hotmail.com>
2024-09-05 21:08:28 +0200

    fix SQL constraint problem in delete user method

--- src/main/java/com/cisowski/schoolmanagement/model/entity/User.java
+++ src/main/java/com/cisowski/schoolmanagement/model/entity/User.java
@@ -34,7 +34,7 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Gender gender;
     @Column(nullable = false)
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE, orphanRemoval = true)
     @JoinTable(name = "users_authorities",
     joinColumns = @JoinColumn(
             name = "user_id", referencedColumnName = "id"),
