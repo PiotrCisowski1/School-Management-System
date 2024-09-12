a125ac34a079957a64b5521c452f5395bca4c297
PiotrCisowski1 <piotr.cisowski@hotmail.com>
2024-09-12 17:49:22 +0200

    add DB logging (Logback) feature for errors and code debug messages

--- src/main/java/com/cisowski/schoolmanagement/SchoolmanagementApplication.java
+++ src/main/java/com/cisowski/schoolmanagement/SchoolmanagementApplication.java
@@ -1,9 +1,14 @@
 package com.cisowski.schoolmanagement;
 
+import com.cisowski.schoolmanagement.utility.DbLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class SchoolmanagementApplication {
 
 	public static void main(String[] args) {
